87b0c35fd0f0689a4c46c7b55659e5161e60d4eb
Ajay Kumar <89543633a@gmail.com>
2024-09-28 18:57:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const PORT = process.env.PORT || 4000;
 
 // Import routes
-const routes = require('./routes/index');
+const routes = require("./routes/index");
 
 // Use CORS middleware to allow requests from any origin
 app.use(cors());
@@ -13,7 +13,26 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Use routes
-app.use('/api', routes);
+app.use("/api", routes);
+
+// to keep app alive in f1 plan
+const keepAlive = () => {
+  const options = {
+    host: "https://helpline-backend.onrender.com", // Replace with your Azure app URL
+    path: "/",
+  };
+
+  https
+    .get(options, (res) => {
+      console.log(`Keep-alive ping responded with: ${res.statusCode}`);
+    })
+    .on("error", (err) => {
+      console.log(`Error in keep-alive request: ${err.message}`);
+    });
+};
+
+// Send a request every 15 minutes to keep the app active
+setInterval(keepAlive, 900000); // 900000 ms = 15 minutes
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
